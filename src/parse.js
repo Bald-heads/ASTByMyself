ce50f5ca6f1d02f14d6083e7acdba78094dc9517
zxr <763183105@qq.com>
2022-08-11 15:15:40 +0800

    Implement the abstract syntax tree

--- src/parse.js
+++ src/parse.js
@@ -9,36 +9,39 @@ export default function parse(templateString) {
     let endRegExp = /^\<\/([a-z]+[1-6]?)\>/
     //Define two stacks one to store labels and one to store content
     let stackTag = []
-    let stackLetter = []
+    let stackLetter = [{children: []}]
     //Grab the text before the end tag
     let wordRegExp = /^([^\<]+)\<\/[a-z]+[1-6]?\>/
     while (index < templateString.length - 1) {
         restString = templateString.substring(index)
         if (startRegExp.test(restString)) {
             let tag = restString.match(startRegExp)[1]
-            //Push the tag and the empty array onto the stack, respectively
+            //Push the tag and the object onto the stack, respectively
             stackTag.push(tag)
-            stackLetter.push([])
+            stackLetter.push({'tag': tag, 'children': []})
             index += tag.length + 2
         } else if (endRegExp.test(restString)) {
             let tag = restString.match(endRegExp)[1]
-            if (tag === stackTag[stackTag.length - 1]) {
-                stackTag.pop()
+            let pop_tag = stackTag.pop()
+            let pop_letter = stackLetter.pop()
+            if (tag === pop_tag) {
+                if (stackLetter.length > 0) {
+                    stackLetter[stackLetter.length - 1].children.push(pop_letter)
+                }
             } else {
                 throw new Error(`${stackTag[stackTag.length - 1]} tag is not closed`)
             }
             index += tag.length + 3
         } else if (wordRegExp.test(restString)) {
             let word = restString.match(wordRegExp)[1]
-            if (!/^\s+$/.test(word)){
-                console.log("检测到文字", word)
+            if (!/^\s+$/.test(word)) {
+                //Text is detected on top of stack
+                stackLetter[stackLetter.length - 1].children.push({"text": word, 'type': 3})
             }
             index += word.length
         } else {
             index++
         }
     }
-    console.log(stackTag, stackLetter)
-    return templateString
-
+    return stackLetter[0].children[0]
 }
